aff6f854695937eb6e9230c0c42ce6a56a4a0cfc
christina-aguiar <123034210+christina-aguiar@users.noreply.github.com>
2023-04-30 02:24:46 +0200

    get player's selection

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log('Goodbye World');
-const computerSelection = ['Rock', 'Paper', 'Scissors']
+const computerSelection = ['Rock', 'Paper', 'Scissors'];
 function getComputerChoice() {
     return computerSelection[Math.floor(Math.random() * 3)]
 }
+let playerSelection = prompt("Make your choice. Rock, paper or scissors?");
