d393a24152ed376a507d69bf369f4cd8ca474561
christina-aguiar <123034210+christina-aguiar@users.noreply.github.com>
2023-05-03 01:22:39 +0200

    keep track of scores and rounds

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
 const choices = ['rock', 'paper', 'scissors'];
 
+playerScore = 0;
+
+computerScore = 0;
+
+round = 0;
+
+function game() {
+    playRound();
+    return `Round = ${round} - Player score = ${playerScore} - Computer score =  ${computerScore}.`
+}
+
 function playRound(playerSelection, computerSelection) {
 
-    playerSelection = prompt("Make your choice. Rock, paper or scissors?").toLowerCase()
+    playerSelection = prompt("Make your choice. Rock, paper or scissors?").toLowerCase();
 
     computerSelection = choices[Math.floor(Math.random() * 3)];
 
+    round = round + 1; 
+    
     if (playerSelection == 'rock' && computerSelection == 'paper') {
-        console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Paper beats rock.`)
+        console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Paper beats rock.`);
+        computerScore = computerScore + 1;
     }
     else if (playerSelection == 'rock' && computerSelection == 'rock') {
         console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}.\nTie!`)
     }
     else if (playerSelection == 'rock' && computerSelection == 'scissors') {
         console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou win! Rock beats scissors.`)
+        playerScore = playerScore + 1;
     }
     else if (playerSelection == 'paper' && computerSelection == 'rock') {
         console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou win! Paper beats rock.`)
+        playerScore = playerScore + 1;
     }
     else if (playerSelection == 'paper' && computerSelection == 'paper') {
         console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nTie!`)
     }
     else if (playerSelection == 'paper' && computerSelection == 'scissors'){
         console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Scissors beat paper.`)
+        computerScore = computerScore + 1;
     }
     else if (playerSelection == 'scissors' && computerSelection == 'rock') {
         console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Rock beats scissors.`)
+        computerScore = computerScore + 1;
     }
     else if(playerSelection == 'scissors' && computerSelection == 'paper') {
         console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou win! Paper beats scissors.`)
+        playerScore = playerScore + 1;
     }
     else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
         console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nTie!`)
