dabef565ed7dcad7fa9a6e85a5309c00c84ae94e
christina-aguiar <123034210+christina-aguiar@users.noreply.github.com>
2023-05-03 00:50:34 +0200

    bug fix

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
 const choices = ['rock', 'paper', 'scissors'];
 
-function getComputerChoice() {
-     return choices[Math.floor(Math.random() * 3)]
-}
-
-const playerSelection = prompt("Make your choice. Rock, paper or scissors?").toLowerCase();
-
 function playRound(playerSelection, computerSelection) {
+
+    playerSelection = prompt("Make your choice. Rock, paper or scissors?").toLowerCase()
+
+    computerSelection = choices[Math.floor(Math.random() * 3)];
+
     if (playerSelection == 'rock' && computerSelection == 'paper') {
-        return `You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Paper beats rock.`
+        console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Paper beats rock.`)
     }
     else if (playerSelection == 'rock' && computerSelection == 'rock') {
-        return `You chose ${playerSelection}\nComputer chose ${computerSelection}.\nTie!`
+        console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}.\nTie!`)
     }
     else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return `You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou win! Rock beats scissors.`
+        console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou win! Rock beats scissors.`)
     }
     else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return `You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou win! Paper beats rock.`
+        console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou win! Paper beats rock.`)
     }
     else if (playerSelection == 'paper' && computerSelection == 'paper') {
-        return `You chose ${playerSelection}\nComputer chose ${computerSelection}\nTie!`
+        console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nTie!`)
     }
     else if (playerSelection == 'paper' && computerSelection == 'scissors'){
-        return `You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Scissors beat paper.`
+        console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Scissors beat paper.`)
     }
     else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        return `You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Rock beats scissors.`
+        console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Rock beats scissors.`)
     }
     else if(playerSelection == 'scissors' && computerSelection == 'paper') {
-        return `You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou win! Paper beats scissors.`
+        console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou win! Paper beats scissors.`)
     }
     else if (playerSelection == 'scissors' && computerSelection == 'scissors') {
-        return `You chose ${playerSelection}\nComputer chose ${computerSelection}\nTie!`
+        console.log(`You chose ${playerSelection}\nComputer chose ${computerSelection}\nTie!`)
     }
     else {
-        return "Something went wrong!"
+        console.log("Something went wrong!")
     }
 }
 
-const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+
 
