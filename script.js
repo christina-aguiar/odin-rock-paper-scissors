153d268a6a985d227d70b0f0df7078f94ac0ecad
christina-aguiar <123034210+christina-aguiar@users.noreply.github.com>
2023-05-26 01:18:58 +0200

    add reset button

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ let playerScore = 0;
 
 let computerScore = 0;
 
+let playerSelection;
+
+let computerSelection;
+
 const result = document.querySelector('.result');
 
-let score = document.querySelector('.score');
+const score = document.querySelector('.score');
 
-let playerSelection; 
+const restart = document.querySelector('.restart');
+restart.addEventListener("click", () => {
+    playerScore = 0;
+    computerScore = 0;
+    score.classList.remove("final");
+    score.textContent = `Player score = ${playerScore}\r\nComputer score = ${computerScore}`;
+});
+
+const finalResult = document.querySelector('p.final-result');
 
 const rockButton = document.querySelector('button.rock').addEventListener("click", function() {
     playRound('rock');
@@ -27,69 +39,69 @@ function playRound(playerSelection) {
     computerSelection = choices[Math.floor(Math.random() * 3)];
 
     if (playerSelection == 'rock' && computerSelection == 'paper' && computerScore < 5 && playerScore < 5) {
-        result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Paper beats rock.`;
-        updateText();
+        result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nPaper beats rock.`;
+        updateScore();
         computerScore = computerScore + 1;
         
     }
     else if (playerSelection == 'rock' && computerSelection == 'rock' && computerScore < 5 && playerScore < 5) {
         result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}.\nTie!`;
-        updateText();
+        updateScore();
     }
     else if (playerSelection == 'rock' && computerSelection == 'scissors' && computerScore < 5 && playerScore < 5) {
-        result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou win! Rock beats scissors.`;
+        result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nRock beats scissors.`;
         playerScore = playerScore + 1;
-        updateText();
+        updateScore();
     }
     else if (playerSelection == 'paper' && computerSelection == 'rock' && computerScore < 5 && playerScore < 5) {
-        result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou win! Paper beats rock.`;
+        result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nPaper beats rock.`;
         playerScore = playerScore + 1;
-        updateText();
+        updateScore();
     }
     else if (playerSelection == 'paper' && computerSelection == 'paper' && computerScore < 5 && playerScore < 5) {
         result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nTie!`;
-        updateText();
+        updateScore();
     }
     else if (playerSelection == 'paper' && computerSelection == 'scissors' && computerScore < 5 && playerScore < 5){
-        result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Scissors beat paper.`;
+        result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nScissors beat paper.`;
         computerScore = computerScore + 1;
-        updateText();
+        updateScore();
     }
     else if (playerSelection == 'scissors' && computerSelection == 'rock' && computerScore < 5 && playerScore < 5) {
-        result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou lose! Rock beats scissors.`;
+        result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nRock beats scissors.`;
         computerScore = computerScore + 1;
-        updateText();
+        updateScore();
     }
     else if(playerSelection == 'scissors' && computerSelection == 'paper' && computerScore < 5 && playerScore < 5) {
-        result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nYou win! Paper beats scissors.`;
+        result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nPaper beats scissors.`;
         playerScore = playerScore + 1;
-        updateText();
+        updateScore();
     }
     else if (playerSelection == 'scissors' && computerSelection == 'scissors' && computerScore < 5 && playerScore < 5) {
         result.textContent = `You chose ${playerSelection}\nComputer chose ${computerSelection}\nTie!`;
-        updateText();
+        updateScore();
+    }
+    else if (playerScore >= 5 || computerScore >= 5) {
+        updateScore()
     }
     else {
         result.textContent = "something went wrong"
-        updateText()
+        updateScore()
     }
 }
 
-function updateText() {
-    if (playerScore < 5 && computerScore < 5) {
-        score.textContent = `Player score = ${playerScore}\r\nComputer score = ${computerScore}`;
+function updateScore() {
+    if (computerScore >=5) {
+        score.classList.add("final")
+        score.textContent = `Player score = ${playerScore}\r\nComputer score = ${computerScore}\nYOU LOST!`;
+        restart.classList.remove("hide")
     }
     else if (playerScore >= 5) {
-        score.textContent = `Player score = ${playerScore}\r\nComputer score = ${computerScore}`
-        result.textContent = "You won"
-        result.classList.add('final')
-    }
-    else if (computerScore >= 5) {
-        score.textContent = `Player score = ${playerScore}\r\nComputer score = ${computerScore}`
-        result.textContent = "You lost"
-        result.classList.add('final')
+        score.classList.add("final")
+        score.textContent = `Player score = ${playerScore}\r\nComputer score = ${computerScore}\nYOU WON!`;
+        restart.classList.remove('hide')
     }
     else {
-        return
+        score.textContent = `Player score = ${playerScore}\r\nComputer score = ${computerScore}`;
     }
 }
